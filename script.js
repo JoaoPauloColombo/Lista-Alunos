1f2530c694d2649ffac9a36dbfda3ffddb75a4b0
Ju4uz <joaosouzapaulo446@gmail.com>
2024-03-05 22:08:52 -0300

    1.1

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const listaAlunos = require("./views/listaAlunos");
 
 app.use(express.static("public"));
 
-app.get('/listaAlunos', (req,res,next)=>{
-    //responder com html
-    res.end(listaAlunos())
+app.get('/listaAlunos', (req,res)=>{
+
+    res.send(listaAlunos())
 
 })
 
 
-app.listen(3002, ()=>{
+app.listen(3001, ()=>{
     console.log("Servidor no ar com express")
 })
+//Para executar o arquivo, escreva node e ("o nome do java script").
