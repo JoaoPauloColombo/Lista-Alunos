07b598a61b65a2875875eed774366f0d58c4b81d
João Paulo <joaosouzapaulo446@gmail.com>
2024-03-06 14:26:13 -0300

    1.3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-// const listaAlunos = require("./views/listaAlunos");
+// const listaAlunos = require("./src/views/listaAlunos");
 
 app.use(express.static("public"));
 
