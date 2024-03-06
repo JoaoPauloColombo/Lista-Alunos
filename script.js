39681a85963f08ac940ca9645cb0e66e9c35d532
Ju4uz <joaosouzapaulo446@gmail.com>
2024-03-05 22:20:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 const express = require("express");
 const app = express();
 
-const listaAlunos = require("./views/listaAlunos");
+// const listaAlunos = require("./views/listaAlunos");
 
 app.use(express.static("public"));
 
-app.get('/listaAlunos', (req,res)=>{
+const listaAlunos = [
+    { 
+        id: 1, 
+        nome: 'João', 
+        idade: 20, 
+        curso: 'Engenharia', 
+        cidade: 'São Paulo' 
+    },
+    { 
+        id: 2, 
+        nome: 'Maria', 
+        idade: 22, 
+        curso: 'Medicina', 
+        cidade: 'Rio de Janeiro' 
+    },
+    { 
+        id: 3, 
+        nome: 'Pedro', 
+        idade: 21, 
+        curso: 'Administração', 
+        cidade: 'Belo Horizonte' 
+    },
+    { 
+        id: 4, 
+        nome: 'Ana', 
+        idade: 19, 
+        curso: 'Direito', 
+        cidade: 'Salvador' 
+    }
+];
 
-    res.send(listaAlunos())
+// app.get('/listaAlunos', (req,res)=>{
 
-})
+//     res.send(listaAlunos())
 
+// })
+
+app.get('/listaAlunos', (req, res) => {
+    res.json(listaAlunos);
+});
 
 app.listen(3001, ()=>{
     console.log("Servidor no ar com express")
