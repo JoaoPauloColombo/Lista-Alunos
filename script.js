618bbbf42a6fb768febf95286648915857e54cb2
João Paulo <joaosouzapaulo446@gmail.com>
2024-03-06 14:25:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,36 +5,36 @@ const app = express();
 
 app.use(express.static("public"));
 
-const listaAlunos = [
-    { 
-        id: 1, 
-        nome: 'João', 
-        idade: 20, 
-        curso: 'Engenharia', 
-        cidade: 'São Paulo' 
-    },
-    { 
-        id: 2, 
-        nome: 'Maria', 
-        idade: 22, 
-        curso: 'Medicina', 
-        cidade: 'Rio de Janeiro' 
-    },
-    { 
-        id: 3, 
-        nome: 'Pedro', 
-        idade: 21, 
-        curso: 'Administração', 
-        cidade: 'Belo Horizonte' 
-    },
-    { 
-        id: 4, 
-        nome: 'Ana', 
-        idade: 19, 
-        curso: 'Direito', 
-        cidade: 'Bahia' 
-    }
-];
+// const listaAlunos = [
+//     { 
+//         id: 1, 
+//         nome: 'João', 
+//         idade: 20, 
+//         curso: 'Engenharia', 
+//         cidade: 'São Paulo' 
+//     },
+//     { 
+//         id: 2, 
+//         nome: 'Maria', 
+//         idade: 22, 
+//         curso: 'Medicina', 
+//         cidade: 'Rio de Janeiro' 
+//     },
+//     { 
+//         id: 3, 
+//         nome: 'Pedro', 
+//         idade: 21, 
+//         curso: 'Administração', 
+//         cidade: 'Belo Horizonte' 
+//     },
+//     { 
+//         id: 4, 
+//         nome: 'Ana', 
+//         idade: 19, 
+//         curso: 'Direito', 
+//         cidade: 'Bahia' 
+//     }
+// ];
 
 // app.get('/listaAlunos', (req,res)=>{
 
@@ -42,11 +42,21 @@ const listaAlunos = [
 
 // })
 
+// app.get('/listaAlunos', (req, res) => {
+//     res.json(listaAlunos);
+// });
+
 app.get('/listaAlunos', (req, res) => {
-    res.json(listaAlunos);
+    let arrAluno = ["Ana", "Carlos", "Renato"];
+    let texto="";
+    for (i = 0; i < arrAluno.length; i++) {
+        texto += i + ":" + arrAluno[i] + "<br>";
+    }
+        res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});
+        res.end(texto);
 });
 
 app.listen(3001, ()=>{
     console.log("Servidor no ar com express")
 })
-//Para executar o arquivo, escreva node e ("o nome do java script").
+//Para executar o arquivo, escreva node e => ("o nome do java script").
