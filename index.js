1ba10ec2e91da16261eae5a560e8b09fb382adb8
João Paulo <joaosouzapaulo446@gmail.com>
2024-03-13 16:48:10 -0300

    1.7

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require('body-parser');
+
 app.use(express.static("public"));
 
-const routes = require('./src/routes/routes.js')
-app.use('/',routes)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+const routes = require('./src/routes/routes.js');
+app.use('/',routes);
 
 app.listen(3002, ()=>{
     console.log("Servidor no ar com express")
-})
+});
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static("public"));
 
+const fs = require("fs");
+
 const router = express.Router();
 
 const listaAlunos = require("../views/listaAlunos.js");
@@ -35,32 +37,43 @@ router.get('/alunoLista', (req, res) => {
     res.send(listaAlunos());
 });
 
-router.post('/addAlunos', (req, res) => {
-    const { name } = req.bodyParser;
-    app.use(bodyParser.json())
-    arrAluno.push({ name });
-    res.send("Aluno adicionado com sucesso!");
-
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end("<h1>Usuario ja foi cadastrado</h1>");
-});
-
-router.get('/novoAluno/:name', (req,resp) => {
+router.get('/novoAluno/:name', (req, resp) => {
     const { name } = req.params;
 
     const index = arrAluno.map(aluno => aluno.name).indexOf(name);
 
-    if(index === -1){
-        let novoNome = {name: name};
+    if (index === -1) {
+        let novoNome = { name: name };
         arrAluno.push(novoNome);
         resp.send(arrAluno);
-    }else{
+    } else {
         resp.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
         resp.end("<h1>Usuario ja foi cadastrado</h1>");
     }
-
 })
 
+router.get('/deletarAluno/:name', (req, resp) => {
+    const { name } = req.params;
+
+    const index = arrAluno.map(aluno => aluno.name).indexOf(name);
+
+    if (index > -1) {
+        arrAluno.splice(index, 1);
+    }
+    resp.send(arrAluno);
+})
+
+router.post('/addAluno', (req, resp) => {
+
+    let dado = req.body;
+    resp.send(JSON.stringify(dado));
+
+    inserirDados(JSON.stringify(dado));
+});
+
+const inserirDados = (dados, err) =>{
+    
+}
 
 module.exports = router;
 
